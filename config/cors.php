683c6547f00eacbaa80f903a7bba0a6374dcb0d0
ademirrocha <tiademir.rocha93@gmail.com>
2021-01-07 00:37:02 -0300

    Correçao cpnfig

--- config/cors.php
+++ config/cors.php
@@ -25,9 +25,9 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['Authorization'],
+    'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => false,
 
